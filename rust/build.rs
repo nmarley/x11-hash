5dd3336b92fbe347a015ab87a04cdbca2116c746
Nathan Marley <nathan@glowberrylabs.com>
2026-01-02 23:13:27 -0800

    Use rust cc crate to static bundle

--- rust/build.rs
+++ rust/build.rs
@@ -1,5 +1,27 @@
 fn main() {
-    println!("cargo:rustc-link-search=../");
-    println!("cargo:rustc-link-lib=static=x11");
-    println!("cargo:rerun-if-changed=../libx11.a");
+    // Compile all C sources directly into the Rust library
+    cc::Build::new()
+        .file("../sha3/blake.c")
+        .file("../sha3/bmw.c")
+        .file("../sha3/cubehash.c")
+        .file("../sha3/echo.c")
+        .file("../sha3/groestl.c")
+        .file("../sha3/jh.c")
+        .file("../sha3/keccak.c")
+        .file("../sha3/luffa.c")
+        .file("../sha3/shavite.c")
+        .file("../sha3/simd.c")
+        .file("../sha3/skein.c")
+        .file("../x11.c")
+        .file("../util.c")
+        .include("../")
+        .include("../sha3")
+        .compile("x11");
+
+    // Trigger rebuild if C sources change
+    println!("cargo:rerun-if-changed=../x11.c");
+    println!("cargo:rerun-if-changed=../x11.h");
+    println!("cargo:rerun-if-changed=../util.c");
+    println!("cargo:rerun-if-changed=../util.h");
+    println!("cargo:rerun-if-changed=../sha3/");
 }
